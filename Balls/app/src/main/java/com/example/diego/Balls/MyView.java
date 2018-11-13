0c01afe4f3c351ccca5bcdb0569118b3508901f8
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2018-11-13 17:05:38 +1100

    first android studio projects

---
+++ Balls/app/src/main/java/com/example/diego/Balls/MyView.java
@@ -0,0 +1,63 @@
+package com.example.diego.Balls;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+
+public class MyView extends View implements Runnable {
+    int speed = 5;
+    int xdirection=speed;
+    int ydirection=2*speed;
+    int x = 100;
+    int y = 100;
+
+    public MyView (Context context){
+        super (context);
+    }
+
+    Thread t=new Thread();
+    Paint paint=new Paint();
+
+    public void start()
+    {
+        t.start();
+    }
+    public void run()
+    {
+        try
+        {
+            Thread.sleep(1000, 1);
+        }catch(InterruptedException e){}
+    }
+
+    @Override
+    protected void onDraw (Canvas canvas){
+        super.onDraw(canvas);
+        int width = getWidth();
+        int height = getHeight();
+        int radius = 50;
+
+
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.WHITE);
+        canvas.drawPaint(paint);
+        paint.setColor(Color.RED);
+        if (x>=width-(radius)  || x<=radius){
+            xdirection = xdirection*(-1);
+        }
+        if (y>=height-(radius)  || y<=radius){
+            ydirection = ydirection*(-1);
+        }
+
+
+        canvas.drawCircle  (x,y,radius,paint);
+        x=x+xdirection;
+        y=y+ydirection;
+        invalidate();
+    }
+}
+
+
---
+++ RedCircle/app/src/main/java/com/example/diego/redcircle/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.diego.redcircle;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MyView(this));
+    }
+
+
+}
